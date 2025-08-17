972b697ad23ba8720d9f6d13b5532e5534b45277
Rudra627 <rudrasahu9007@gmail.com>
2025-08-17 23:24:14 +0530

    By rudra on 17AUg 2025

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -8,6 +8,7 @@ export default function Contact() {
       <p>If you have any questions or would like to get in touch, feel free to reach out!</p>
       <ul>
         <li>Email: rudrasahu9007@gmail.com</li>
+        <li>Instagram:Rudra Narayan Sahu</li>
         <li>LinkedIn: <a href="https://www.linkedin.com/in/rudra-narayan-sahu-9b0a6b1b2/">LinkedIn Profile</a></li>
         <li>ph NO=9668332924</li>
       </ul>
